f1413318ab25a9a09f7ee01106486b8d8bfafa2f
Edis Sijaric <Edis.Sijaric@endava.com>
2022-09-20 15:33:39 +0200

    components, routes and contexts

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+
+
+import {
+  signInWithGooglePopup,
+  signInAuthUserWithEmailAndPassword
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+}
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({
+      ...formFields,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+   await signInAuthUserWithEmailAndPassword(email, password);
+    resetFormFields();
+    } catch (error) {
+        switch(error.code) {
+          case 'auth/wrong-password':
+            alert('Incorrect password for given email');
+            break;
+          case 'auth/user-not-found': 
+            alert('User not found');
+            break;
+          default: console.log(error);
+        }
+    }
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+       <div className="buttons-container">
+        <Button type="submit">
+            Sign In
+          </Button>
+          <Button buttonType="google" onClick={signInWithGoogle} type="button">
+            Sign In With Google
+          </Button>
+       </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,78 @@
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth, 
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDZFrg_gyk0DDbwOl-qrS7TM9Ia2V0gSQM",
+    authDomain: "crown-clothing-db-51d8a.firebaseapp.com",
+    projectId: "crown-clothing-db-51d8a",
+    storageBucket: "crown-clothing-db-51d8a.appspot.com",
+    messagingSenderId: "759979181221",
+    appId: "1:759979181221:web:b8ec30095f6255d2c5794c"
+  };
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if(!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      })
+      
+    } catch (error) {
+      console.log('error creating user', error);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
