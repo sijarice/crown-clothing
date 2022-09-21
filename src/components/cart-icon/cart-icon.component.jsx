52c575ddc2bbbab7a94afab491e646af7a8ce3b2
Edis Sijaric <Edis.Sijaric@endava.com>
2022-09-21 11:31:38 +0200

    cart items quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,16 @@ import { CartContext } from "../../contexts/cart.context";
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+
+    // const quantity = cartItems.reduce((accumulator, currentValue) => accumulator + currentValue.quantity, 0);
 
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
